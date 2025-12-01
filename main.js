bcc1662623ad6e11de216f498d8edb9cfa8ce818
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-12-01 22:37:49 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1. Sum of an Array
+
+// Task: Given an array of numbers, return the sum of all elements.
+// Example: [1, 2, 3] → 6
+
+// function findSumOfArray(array) {
+//   let ans = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     ans = ans + array[i];
+//   }
+//   console.log(ans);
+// }
+// findSumOfArray([1, 2, 3, 4, 5, 6]);
+
+// 2. Count Occurrences
+
+// Task: Given a string and a character, return how many times that character appears.
+// Example: "hello", 'l' → 2
+
+function calculateChars(char) {}
+calculateChars("hello");
