92fe40fdd6f677d14609d2a210023951628d7943
NikolozKapanadzee <nkapanadze369@gmail.com>
2026-01-19 23:22:55 +0400

    feat: new algorithm

--- main.js
+++ main.js
@@ -2248,3 +2248,17 @@
 // }
 
 // console.log(twoSum([2, 7, 11, 15], 9));
+
+// function findLongestWord(str) {
+//   const words = str.split(" ");
+//   let longestWord = "";
+//   for (let word of words) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+
+//   return longestWord;
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
